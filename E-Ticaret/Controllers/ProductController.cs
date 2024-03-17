809e32106766a5379b65265e2709c32b48c08b46
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2024-03-17 14:59:53 +0300

    Yönetim1

---
+++ E-Ticaret/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Ticaret.Controllers
+{
+    public class ProductController : BaseController
+    {
+        // GET: Product
+        public ActionResult i()
+        {
+            if (IsLogon() == false) return RedirectToAction("index", "i");
+            else if ((int)(CurrentUser().MemberType) < 4)
+            {
+                return RedirectToAction("i", "Product");
+            }
+
+            var products = context.Products.ToList();
+
+            return View(products);
+        }
+    }
+}

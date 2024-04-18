88d8bc11a1c3f4c5f1ef4daaf86aeaaa32cfaa10
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2024-04-18 22:46:55 +0300

    yeni ürün

--- E-Ticaret/Controllers/ProductController.cs
+++ E-Ticaret/Controllers/ProductController.cs
@@ -18,12 +18,12 @@ namespace E_Ticaret.Controllers
                 return RedirectToAction("i", "Product");
             }
 
-            var products = context.Products.Where(x=>x.IsDeleted==false||x.IsDeleted==null).ToList();
+            var products = context.Products.Where(x => x.IsDeleted == false || x.IsDeleted == null).ToList();
 
-            return View(products);
+            return View(products.OrderByDescending(x => x.AddedDate).ToList());
         }
         [HttpGet]
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int id = 0)
         {
             var product = context.Products.FirstOrDefault(x => x.Id == id);
 
@@ -37,38 +37,33 @@ namespace E_Ticaret.Controllers
             return View(product);
         }
         [HttpPost]
-        public ActionResult Edit(DB.Products products)
+        public ActionResult Edit(DB.Products product)
         {
             var productImagePath = string.Empty;
-            products.ProductImageName = string.Empty;
-
             if (Request.Files != null && Request.Files.Count > 0)
             {
                 var file = Request.Files[0];
                 if (file.ContentLength > 0)
                 {
-                    var folder = Server.MapPath("~/İmages/Upload/Product");
+                    var folder = Server.MapPath("~/images/upload/Product");
                     var fileName = Guid.NewGuid() + ".jpg";
                     file.SaveAs(Path.Combine(folder, fileName));
 
-                    var filePath = "İmages/Uppload/Product/" + fileName;
+                    var filePath = "images/upload/Product/" + fileName;
                     productImagePath = filePath;
                 }
             }
-
-            if (products.Id > 0)
+            if (product.Id > 0)
             {
-
-                var dbProduct = context.Products.FirstOrDefault(x => x.Id == products.Id);
-
-                dbProduct.Category_Id = products.Category_Id;
-                dbProduct.ModifiedDate = products.ModifiedDate;
-                dbProduct.Description = products.Description;
-                dbProduct.IsContinued = products.IsContinued;
-                dbProduct.Name = products.Name;
-                dbProduct.Price = products.Price;
-                dbProduct.UnitsInStock = products.UnitsInStock;
-                products.IsDeleted = false;
+                var dbProduct = context.Products.FirstOrDefault(x => x.Id == product.Id);
+                dbProduct.Category_Id = product.Category_Id;
+                dbProduct.ModifiedDate = DateTime.Now;
+                dbProduct.Description = product.Description;
+                dbProduct.IsContinued = product.IsContinued;
+                dbProduct.Name = product.Name;
+                dbProduct.Price = product.Price;
+                dbProduct.UnitsInStock = product.UnitsInStock;
+                dbProduct.IsDeleted = false;
                 if (string.IsNullOrEmpty(productImagePath) == false)
                 {
                     dbProduct.ProductImageName = productImagePath;
@@ -76,20 +71,69 @@ namespace E_Ticaret.Controllers
             }
             else
             {
-                products.AddedDate = DateTime.Now;
-                products.IsDeleted= false;
-                products.ProductImageName = productImagePath;
+                product.AddedDate = DateTime.Now;
+                product.IsDeleted = false;
+                product.ProductImageName = productImagePath;
+                context.Entry(product).State = System.Data.Entity.EntityState.Added;
 
-                context.Entry(products).State = System.Data.Entity.EntityState.Added;
             }
-
-
-
 
             context.SaveChanges();
 
-
             return RedirectToAction("i");
+            #region eski
+            //var productImagePath = string.Empty;
+            ////products.ProductImageName = string.Empty;
+
+            //if (Request.Files != null && Request.Files.Count > 0)
+            //{
+            //    var file = Request.Files[0];
+            //    if (file.ContentLength > 0)
+            //    {
+            //        var folder = Server.MapPath("~/İmages/Upload/Product");
+            //        var fileName = Guid.NewGuid() + ".jpg";
+            //        file.SaveAs(Path.Combine(folder, fileName));
+
+            //        var filePath = "İmages/Uppload/Product/" + fileName;
+            //        productImagePath = filePath;
+            //    }
+            //}
+
+            //if (products.Id > 0)
+            //{
+
+            //    var dbProduct = context.Products.FirstOrDefault(x => x.Id == products.Id);
+
+            //    dbProduct.Category_Id = products.Category_Id;
+            //    dbProduct.ModifiedDate = products.ModifiedDate;
+            //    dbProduct.Description = products.Description;
+            //    dbProduct.IsContinued = products.IsContinued;
+            //    dbProduct.Name = products.Name;
+            //    dbProduct.Price = products.Price;
+            //    dbProduct.UnitsInStock = products.UnitsInStock;
+            //    products.IsDeleted = false;
+            //    if (string.IsNullOrEmpty(productImagePath) == false)
+            //    {
+            //        dbProduct.ProductImageName = productImagePath;
+            //    }
+            //}
+            //else
+            //{
+            //    products.AddedDate = DateTime.Now;
+            //    products.IsDeleted = false;
+            //    products.ProductImageName = productImagePath;
+
+            //    context.Entry(products).State = System.Data.Entity.EntityState.Added;
+            //}
+
+
+
+
+            //context.SaveChanges();
+
+
+            //return RedirectToAction("i"); 
+            #endregion
         }
 
         public ActionResult Delete(int id)
--- E-Ticaret/Controllers/iController.cs
+++ E-Ticaret/Controllers/iController.cs
@@ -19,7 +19,7 @@ namespace E_Ticaret.Controllers
         public ActionResult Index(int id = 0)
         {
             //context.Members.ToList();
-            IQueryable<DB.Products> products = context.Products.Where(x => x.IsDeleted == false || x.IsDeleted == null);
+            IQueryable<DB.Products> products = context.Products.OrderByDescending(x=>x.AddedDate).Where(x => x.IsDeleted == false || x.IsDeleted == null);
             DB.Categories category = null;
             if (id > 0)
             {
